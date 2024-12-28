cdbf965efd9b2b618c765146400a43b11aca7f4d
Andrey <kms.amur@gmail.com>
2024-12-29 02:47:03 +1000

    fix

--- github-scan-dir.sh
+++ github-scan-dir.sh
@@ -9,43 +9,33 @@ fi
 USER="$1"
 REPO="$2"
 URL="https://github.com/$USER/$REPO/"
+USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0"
+HREF="href=\"\/${USER}\/${REPO}\/(blob|tree)"
+SED_STR='s/href="/\\e[37mFile:\\e[0m https:\/\/raw.githubusercontent.com/g ; s/blob\///g ; s/.$//g ; s/%20/ /g ; s/.*tree\/main\//\\e[33mFolder:\\e[0m /'
 
 function request {
-url="$1"
-user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0"
-response=$(curl -s --max-time 7 -A "${user_agent}" "${url}")
-if ! [[ "${response}" ]]; then
-  sleep 3
-  request
-fi
+response=$(curl -s --max-time 7 -A "${USER_AGENT}" "$1")
 echo "${response}"
 }
 
 function directory {
-dir=$(echo $(request "${URL}") | grep -oP "href=\"\/${USER}\/${REPO}\/(blob|tree)[^\"]+\"" | \
-sed 's/href="/\\e[37mFile:\\e[0m https:\/\/raw.githubusercontent.com/g ; s/blob\///g ; s/.$//g ; s/%20/ /g ; s/.*tree\/main\//\\e[33mFolder:\\e[0m /' | \
-sort | uniq)
+output=$(request "$1")
+dir=$(echo "${output}" | grep -oP "$2" | sed "${SED_STR}" | sort | uniq)
 echo "${dir}"
 }
 
-function sub_directory {
-output=$(request "${URL}tree/main/$1")
-dir=$(echo "${output}" | grep -oP "href=\"\/${USER}\/${REPO}\/(blob|tree)\/main\/$1[^\"]+\"" | \
-sed 's/href="/\\e[37mFile:\\e[0m https:\/\/raw.githubusercontent.com/g ; s/blob\///g ; s/.$//g ; s/%20/ /g ; s/.*tree\/main\//\\e[33mFolder:\\e[0m /' | \
-sort | uniq)
-echo "${dir}"
-}
-
-trees=$(directory)
+trees=$(directory "${URL}" "${HREF}[^\"]+\"")
 echo -e "User: \e[37m${USER}\e[0m  Repository: \e[37m${REPO}\e[0m\n${trees}"
 
-for line in $(echo ${trees} | sed 's/\\e\[33mFolder:\\e\[0m /\n/g ; s/\\e\[37mFile:\\e\[0m /\n/g ; s/ /%20/g'); do
+sed_str_sub='s/\\e\[33mFolder:\\e\[0m /\n/g ; s/\\e\[37mFile:\\e\[0m /\n/g ; s/ /%20/g'
+for line in $(echo ${trees} | sed "${sed_str_sub}"); do
   if ! [[ "$line" =~ ^(https) ]]; then
   folder=$(echo "${line}" | sed 's/...$//')
-  sub_trees=$(sub_directory "${folder}")
+  GREP_STR="${HREF}\/main\/${folder}[^\"]+\""
+  sub_trees=$(directory "${URL}tree/main/${folder}" "${GREP_STR}")
   while ! [[ "$sub_trees" ]]; do
-      sleep 2
-      sub_trees=$(sub_directory "${folder}")
+      sleep 1
+      sub_trees=$(directory "${URL}tree/main/${folder}" "${GREP_STR}")
     done
   space_line=$(echo "${line}" | sed 's/%20/ /g')
   echo -e "  *** \e[33m${space_line}\e[0m ***\n${sub_trees}"
